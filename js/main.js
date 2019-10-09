5116ad4491874bbad77097473abb271c453604bf
brassta <brassta@gmail.com>
2019-10-09 21:10:52 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Web:  www.agenzzia.com
+ * Author: Branko Stevanovic (branko@agenzzia.com)
+ * Date: 09-Oct-19
+ * Time: 20:31
+ */
+"use strict";
+(function() {
+    // document.querySelector('.wrapper').ontouchend = (e) => {
+    //     e.preventDefault();
+    // }
+    // document.querySelector('.form-wrapper').ontouchend = (e) => {
+    //     console.log('vidimo se ovde');
+    // }
+}())
